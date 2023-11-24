4f42f6892c8493e1a526523ce75c646db53305c1
dingao <114223164+dingding0908@users.noreply.github.com>
2023-11-24 16:17:46 +0800

    大豆病虫害识别,模型识别能力和jupyter不一样，待修改！！！

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from common import res_success, get_all_params, md5_file, save_uploaded_file, res_error
+from common import res_success, get_all_params, md5_file, save_uploaded_file, res_error, idenftify_disease
 import os
 from sqlalchemy import create_engine, text
 from flask_cors import CORS
@@ -36,10 +36,11 @@ def get_soybeanharmful():
             data = dict(rows[0])
     return app.success(data)
 
+
 @app.route('/identifyharmful/', methods=["POST"])
-def get_soybeanharmful():
+def identify_soybeanharmful():
     if request.method == 'POST':
-        rootpath = '\\\DESKTOP-OSKGC5P\GISDATA_share\upload'
+        rootpath = r'\\DESKTOP-OSKGC5P\GISDATA_share\upload'
         file = request.files['image_url']
         img_md5 = md5_file(file)
         file.seek(0)  # 生成MD5时候图片索引读取到最后，重新指向图片开头，保存文件。
@@ -48,16 +49,20 @@ def get_soybeanharmful():
         img_path = os.path.join(upload_folder.split('DESKTOP-OSKGC5P')[1][1:],
                                 img_md5 + '.' + file.filename.split('.')[-1])
         save_uploaded_file(file, upload_folder, img_md5 + '.' + file.filename.split('.')[-1])
-        # img_path
-        # sql_query = text("SELECT * FROM harmful.newtable WHERE img_md5='{img_md5}'".format(img_md5=img_md5))
-        # result = connection.execute(sql_query)
-        # rows = result.fetchall()
-        # result.close()
-        # if len(rows) == 0:
-        #     data = '无法识别！！！'
-        #     return app.error(data)
-        # else:
-        #     data = dict(rows[0])
+        print('img-path:',os.path.join(r'D:/',img_path).replace('\\','/'))
+        class_name = idenftify_disease(os.path.join(r'D:/',img_path).replace('\\','/'))
+        print('class_name',class_name)
+        sql_query = text("SELECT * FROM harmful.newtable WHERE harmful_name='{class_name}'".format(class_name=class_name))
+        result = connection.execute(sql_query)
+        rows = result.fetchall()
+        result.close()
+        if len(rows) == 0:
+            data = '无法识别！！！'
+            return app.error(data)
+        else:
+            data = dict(rows[0])
+            del data['img_path']
+            del data['img_md5']
     return app.success(data)
 
 if __name__ == '__main__':
--- common.py
+++ common.py
@@ -36,9 +36,25 @@ def res_error(data=[], msg='失败', code=500):
             msg = "通用错误"
     return jsonify({"code": code, "msg": msg, "message": msg, "data": data})
 
-# import tensorflow as tf
-# from tensorflow import keras
-# from tensorflow.keras import layers
-# from matplotlib import pyplot as plt
-# from IPython.display import clear_output
-# import tensorflow_hub as hub
+import tensorflow as tf
+import tensorflow_hub as hub
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from tensorflow.keras.applications.resnet50 import  preprocess_input, decode_predictions
+def idenftify_disease(img_path):
+    custom_objects={'KerasLayer': hub.KerasLayer}
+    saved_model_path = r"E:\Desktop\GIT\datasciencestudy\17 DeepLearning\renn50_1123.h5"  # 将路径替换为你实际保存模型的路径
+    model=keras.models.load_model(saved_model_path,custom_objects=custom_objects)
+    class_names = ['MossaicVirus','SouthernBlight','SuddenDeathSyndrone','YellowMosaic','bacterial_blight','brown_spot','crestamento','ferrugen','powdery_mildew','septoria']
+    img = image.load_img(img_path, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
+    predictions = model.predict(img_array)
+    max_index = np.argmax(predictions)
+    print('最大值：',predictions)
+    if predictions.max()<0.4:
+        return 'other_harmful'
+    return class_names[max_index]

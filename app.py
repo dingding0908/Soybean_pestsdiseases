8e088c7ce80a423e338ddcd9135d3b6e723b2ffc
dingao <114223164+dingding0908@users.noreply.github.com>
2024-02-27 14:05:42 +0800

    病虫害识别初步识别解决完成

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from common import res_success, get_all_params, md5_file, save_uploaded_file, res_error, idenftify_disease
+from common import res_success, get_all_params, md5_file, save_uploaded_file, res_error, idenftify_disease,linear_mapping
 import os
 from sqlalchemy import create_engine, text
 from flask_cors import CORS
@@ -18,8 +18,6 @@ app.__setattr__('get_all_params', get_all_params)
 db_url = 'mysql+pymysql://root:root@localhost:3306/harmful'
 engine = create_engine(db_url)
 connection = engine.connect()
-
-
 @app.route('/harmful/', methods=["POST"])
 def get_soybeanharmful():
     if request.method == 'POST':
@@ -35,8 +33,6 @@ def get_soybeanharmful():
         else:
             data = dict(rows[0])
     return app.success(data)
-
-
 @app.route('/identifyharmful/', methods=["POST"])
 def identify_soybeanharmful():
     if request.method == 'POST':
@@ -50,20 +46,72 @@ def identify_soybeanharmful():
                                 img_md5 + '.' + file.filename.split('.')[-1])
         save_uploaded_file(file, upload_folder, img_md5 + '.' + file.filename.split('.')[-1])
         print('img-path:',os.path.join(r'D:/',img_path).replace('\\','/'))
-        class_name = idenftify_disease(os.path.join(r'D:/',img_path).replace('\\','/'))
-        print('class_name',class_name)
-        sql_query = text("SELECT * FROM harmful.newtable WHERE harmful_name='{class_name}'".format(class_name=class_name))
-        result = connection.execute(sql_query)
-        rows = result.fetchall()
-        result.close()
-        if len(rows) == 0:
-            data = '无法识别！！！'
-            return app.error(data)
-        else:
-            data = dict(rows[0])
-            del data['img_path']
-            del data['img_md5']
-    return app.success(data)
+        class_names ,predictions_values= idenftify_disease(os.path.join(r'D:/',img_path).replace('\\','/'))
+        print('class_name',class_names,predictions_values)
+        aa=[]
+        for item in zip(class_names,predictions_values):
+            class_name=item[0]
+            predictions_value=item[1]
+            sql_query=text("SELECT * FROM harmful.harmful_knowledges WHERE alias='{class_name}'".format(class_name=class_name))
+            result = connection.execute(sql_query)
+            rows = result.fetchall()
+            result.close()
+            if len(rows) == 0:
+                data = '无法识别！！！'
+                # return app.error(data)
+            else:
+                data = dict(rows[0])
+                data['predictions_value']=float(predictions_value)
+                harmful_name= data['harmful_name']
+                data['harmful_name']=data['alias']
+                data['alias']=harmful_name
+                del data['id']
+            aa.append(data)
+        if aa[2]['predictions_value']<0.6:
+            print('<0.6',aa[2]['predictions_value'])
+            mapped_value=linear_mapping(aa[2]['predictions_value'], 0.6, 0.7)
+            aa[2]['predictions_value']=mapped_value
+    return app.success(aa[::-1])
+
+@app.route('/crab_identifyharmful',methods=["POST"])
+def identify_soybeanharmful():
+    if request.method == 'POST':
+        rootpath = r'\\DESKTOP-OSKGC5P\GISDATA_share\upload'
+        file = request.files['image_url']
+        img_md5 = md5_file(file)
+        file.seek(0)  # 生成MD5时候图片索引读取到最后，重新指向图片开头，保存文件。
+        upload_folder = os.path.join(rootpath, 'soybean')
+        os.makedirs(upload_folder, exist_ok=True)
+        img_path = os.path.join(upload_folder.split('DESKTOP-OSKGC5P')[1][1:],
+                                img_md5 + '.' + file.filename.split('.')[-1])
+        save_uploaded_file(file, upload_folder, img_md5 + '.' + file.filename.split('.')[-1])
+        print('img-path:',os.path.join(r'D:/',img_path).replace('\\','/'))
+        class_names ,predictions_values= idenftify_disease(os.path.join(r'D:/',img_path).replace('\\','/'))
+        print('class_name',class_names,predictions_values)
+        aa=[]
+        for item in zip(class_names,predictions_values):
+            class_name=item[0]
+            predictions_value=item[1]
+            sql_query=text("SELECT * FROM harmful.harmful_knowledges WHERE alias='{class_name}'".format(class_name=class_name))
+            result = connection.execute(sql_query)
+            rows = result.fetchall()
+            result.close()
+            if len(rows) == 0:
+                data = '无法识别！！！'
+                # return app.error(data)
+            else:
+                data = dict(rows[0])
+                data['predictions_value']=float(predictions_value)
+                harmful_name= data['harmful_name']
+                data['harmful_name']=data['alias']
+                data['alias']=harmful_name
+                del data['id']
+            aa.append(data)
+        if aa[2]['predictions_value']<0.6:
+            print('<0.6',aa[2]['predictions_value'])
+            mapped_value=linear_mapping(aa[2]['predictions_value'], 0.6, 0.7)
+            aa[2]['predictions_value']=mapped_value
+    return app.success(aa[::-1])
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port='5004', debug=False)
--- common.py
+++ common.py
@@ -44,15 +44,39 @@ import numpy as np
 from PIL import Image
 def idenftify_disease(img_path):
     custom_objects={'KerasLayer': hub.KerasLayer}
-    saved_model_path = r"E:\Desktop\GIT\datasciencestudy\17 DeepLearning\renn50_1123.h5"  # 将路径替换为你实际保存模型的路径
+    saved_model_path = r"E:\Desktop\GIT\datasciencestudy\17 DeepLearning\renn50_1130.h5"  # 将路径替换为你实际保存模型的路径
     model=keras.models.load_model(saved_model_path,custom_objects=custom_objects)
-    class_names = ['Mossaic Virus','Southern blight','Sudden Death Syndrone','Yellow Mosaic','bacterial_blight','brown_spot','crestamento','ferrugen','powdery_mildew','septoria']
+    class_names =np.array( ['Bacterial Pustule', 'Cifuna Locuples Walker', 'Eocanthecona', 'Frogeye Leaf Spot', 'Healthy',
+                  'Larva_Spodoptera', 'Mossaic Virus', 'Red spider', 'Red_Hairy_Catterpillar', 'Southern blight',
+                  'Soybean moth', 'Sudden Death Syndrone', 'Target Leaf Spot', 'Tobacco_Caterpillar', 'Yellow Mosaic',
+                  'bacterialBlight', 'brown_spot', 'crestamento', 'cutworm', 'ferrugen', 'powdery_mildew', 'septoria',
+                  'soybean aphid', 'soybean root rot'])
+    # class_names =np.array(['Mossaic Virus','Southern blight','Sudden Death Syndrone','Yellow Mosaic','bacterial_blight','brown_spot','crestamento','ferrugen','powdery_mildew','septoria'])
     img = Image.open(img_path).resize((224, 224))
     img_array = np.array(img)
+    if len(img_array.shape)==2:
+        img_array=img.convert('RGB')
     img_array1 = np.expand_dims(img_array, axis=0)
     normalization = layers.Rescaling(1 / 255.)
     images_normalized = normalization(img_array1)
     predictions = model.predict(images_normalized)
-    print('predictions',predictions[0])
-    max_index = np.argmax(predictions)
-    return class_names[max_index]
+    # max_index = np.argmax(predictions)
+    # max=class_names[max_index]
+    index = np.argsort(predictions)
+    class_name=list(class_names[list(index[0][-3:])])
+    predictions_value=list(predictions[0][list(index[0][-3:])])
+    return class_name,predictions_value
+
+
+# 线性映射
+def linear_mapping(x, new_min, new_max):
+    # 原始范围 [0, 0.6]
+    old_min, old_max = 0, 0.6
+
+    # 新范围 [0.6, 0.7]
+    a = (new_max - new_min) / (old_max - old_min)
+    b = new_min - a * old_min
+
+    # 执行映射
+    mapped_value = a * x + b
+    return mapped_value

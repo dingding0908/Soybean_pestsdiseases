0c82f9c78d948a631642d00aacff1cbf6e864997
dingao <114223164+dingding0908@users.noreply.github.com>
2024-03-25 16:39:16 +0800

    修改

--- app.py
+++ app.py
@@ -40,49 +40,49 @@ def get_soybeanharmful():
     return app.success(data)
 
 
-# @app.route('/identifyharmful/', methods=["POST"])
-# def identify_soybeanharmful():
-#     if request.method == 'POST':
-#         rootpath = r'\\DESKTOP-OSKGC5P\GISDATA_share\upload'
-#         file = request.files['image_url']
-#         img_md5 = md5_file(file)
-#         file.seek(0)  # 生成MD5时候图片索引读取到最后，重新指向图片开头，保存文件。
-#         upload_folder = os.path.join(rootpath, 'soybean')
-#         os.makedirs(upload_folder, exist_ok=True)
-#         img_path = os.path.join(upload_folder.split('DESKTOP-OSKGC5P')[1][1:],
-#                                 img_md5 + '.' + file.filename.split('.')[-1])
-#         save_uploaded_file(file, upload_folder, img_md5 + '.' + file.filename.split('.')[-1])
-#         print('img-path:', os.path.join(r'D:/', img_path).replace('\\', '/'))
-#         class_names, predictions_values = idenftify_disease(os.path.join(r'D:/', img_path).replace('\\', '/'))
-#         print('class_name', class_names, predictions_values)
-#         aa = []
-#         for item in zip(class_names, predictions_values):
-#             class_name = item[0]
-#             predictions_value = item[1]
-#             sql_query = text(
-#                 "SELECT * FROM harmful.harmful_knowledges WHERE English_name='{class_name}'".format(
-#                     class_name=class_name))
-#             result = connection.execute(sql_query)
-#             rows = result.fetchall()
-#             result.close()
-#             if len(rows) == 0:
-#                 data = '无法识别！！！'
-#                 return app.error(data)
-#             else:
-#                 data = dict(rows[0])
-#                 data['predictions_value'] = round(float(predictions_value), 3)
-#                 harmful_name = data['harmful_name']
-#                 data['harmful_name'] = data['alias']
-#                 data['alias'] = harmful_name
-#                 del data['id']
-#                 del data['bz']
-#                 del data['alias']
-#             if data['predictions_value'] < 0.6:
-#                 print('<0.6', data['predictions_value'])
-#                 mapped_value = linear_mapping(data['predictions_value'], 0.6, 0.7)
-#                 data['predictions_value'] = round(mapped_value, 2)
-#             aa.append(data)
-#     return app.success(aa[::-1])
+@app.route('/soybean_identifyharmful/', methods=["POST"])
+def identify_soybeanharmful():
+    if request.method == 'POST':
+        rootpath = r'\\DESKTOP-OSKGC5P\GISDATA_share\upload'
+        file = request.files['image_url']
+        img_md5 = md5_file(file)
+        file.seek(0)  # 生成MD5时候图片索引读取到最后，重新指向图片开头，保存文件。
+        upload_folder = os.path.join(rootpath, 'soybean')
+        os.makedirs(upload_folder, exist_ok=True)
+        img_path = os.path.join(upload_folder.split('DESKTOP-OSKGC5P')[1][1:],
+                                img_md5 + '.' + file.filename.split('.')[-1])
+        save_uploaded_file(file, upload_folder, img_md5 + '.' + file.filename.split('.')[-1])
+        print('img-path:', os.path.join(r'D:/', img_path).replace('\\', '/'))
+        class_names, predictions_values = idenftify_disease(os.path.join(r'D:/', img_path).replace('\\', '/'))
+        print('class_name', class_names, predictions_values)
+        aa = []
+        for item in zip(class_names, predictions_values):
+            class_name = item[0]
+            predictions_value = item[1]
+            sql_query = text(
+                "SELECT * FROM harmful.harmful_knowledges WHERE English_name='{class_name}'".format(
+                    class_name=class_name))
+            result = connection.execute(sql_query)
+            rows = result.fetchall()
+            result.close()
+            if len(rows) == 0:
+                data = '无法识别！！！'
+                return app.error(data)
+            else:
+                data = dict(rows[0])
+                data['predictions_value'] = round(float(predictions_value), 3)
+                harmful_name = data['harmful_name']
+                data['harmful_name'] = data['alias']
+                data['alias'] = harmful_name
+                del data['id']
+                del data['bz']
+                del data['alias']
+            if data['predictions_value'] < 0.6:
+                print('<0.6', data['predictions_value'])
+                mapped_value = linear_mapping(data['predictions_value'], 0.6, 0.7)
+                data['predictions_value'] = round(mapped_value, 2)
+            aa.append(data)
+    return app.success(aa[::-1])
 
 
 @app.route('/crab_identifyharmful', methods=["POST"])
